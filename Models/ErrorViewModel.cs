2ad7d15acf9622a2ea952f745afc70372425cbe1
wagneruchoa <uchoawagner80@hotmail.com>
2024-12-20 10:12:44 -0400

    Primeiro commit com o projeto base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demo_dot5_5000_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
